cabd4d30010a66920ff75b2a94359139029a2c45
jerryzang <jerryc1214@gmail.com>
2016-01-13 15:52:27 +0800

    add public logo material add index page with scss style fix tsd.d.ts format

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,4 @@
-/// <reference path="angular2-meteor.d.ts" />
+
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angular2-meteor.d.ts" />
 /// <reference path="materialize/materialize.d.ts" />
