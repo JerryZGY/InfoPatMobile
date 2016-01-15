40d49e6772941ec306da103e165976cbcb8cf5e2
jerryzang <jerryc1214@gmail.com>
2016-01-15 18:52:29 +0800

    supplement commit

--- client/main.ts
+++ client/main.ts
@@ -8,4 +8,28 @@ import {RouterLink} from 'angular2/router';
     directives: [RouterLink]
 })
 
-export class Main { }
+export class Main {
+    tw = false;
+    us = false;
+    eu = false;
+    cn = false;
+    jp = false;
+    kr = false;
+    isSearching = false;
+    isSearched = false;
+    
+    reload() {
+        this.isSearching = false;
+        this.isSearched = false;
+        $("#search").focus();
+    }
+    
+    search(text) {
+        $("#search").blur();
+        this.isSearching = true;
+        setTimeout(() => {
+            this.isSearching = false;
+            this.isSearched = true;
+        }, 2000)
+    }
+}
