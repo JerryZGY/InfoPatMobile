ad09d85a37cda5d6d8667a4e7841c1afd772c924
jerryzang <jerryc1214@gmail.com>
2016-01-27 17:53:58 +0800

    temp

--- server/main.ts
+++ server/main.ts
@@ -2,7 +2,7 @@ import {Results} from 'collections/results';
 import {RemoteServer} from 'server/lib/esAPI';
 
 // new RemoteServer().search("test", (data) => Results.update(Results.findOne() || {},  { $set: data }, { upsert: true }));
-Meteor.publish('results', () => { return Results.find() });
+Meteor.publish('results', () => Results.find());
 
 Meteor.methods({
     // search: (text: string, country?: string[]) => new RemoteServer().search(text, (data) => {
