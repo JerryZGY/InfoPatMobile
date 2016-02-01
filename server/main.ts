d347730aee81b7cba19573080405c9652635e337
jerryzang <jerryc1214@gmail.com>
2016-02-01 16:52:23 +0800

    fix mongo initial error fix npm-container initial error

--- server/main.ts
+++ server/main.ts
@@ -2,12 +2,15 @@ import {Results} from 'collections/results';
 import {RemoteServer} from 'server/lib/esAPI';
 import {Logger} from 'lib/logger';
 
+if (Results.find().count() == 0)
+    Results.update(Results.findOne || {}, { $set: new RemoteServer().search("test", []) });
+
 Meteor.publish('results', () => Results.find());
 
 Meteor.methods({
     search(text: string, country: string[]) {
         new Logger().debug(`>>> Search called at ${this.connection.clientAddress}, changedSize: ${
             Results.update(Results.findOne || {}, { $set: new RemoteServer().search(text, country) })
-        }`);
+            }`);
     }
 });
