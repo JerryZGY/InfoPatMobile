9bd5ebc5638b66495582e71192f5354ff5537a29
jerryzang <jerryc1214@gmail.com>
2016-02-17 14:34:50 +0800

    add Meteor Package: mizzao:jquery-ui add comment to main page adjust transition animation between pages

--- client/main.ts
+++ client/main.ts
@@ -7,24 +7,28 @@ import {Analyze} from "client/analyze"
 import {Detail} from "client/detail";
 @Component({ selector: "main", templateUrl: "client/main.html", directives: [RouterLink, Analyze, Detail] })
 export class Main extends MeteorComponent {
-    token: string;
+    // Random id for mongo query.
+    queryToken: string;
+    // Mongo query results.
     results: Mongo.Cursor<IParsedData>;
+    // Aggregations parsed from mongo query result.
     aggs: { [key: string]: ParsedContent[] }[];
-    tw = false;
-    cn = true;
-    us = true;
+    // Country search parameter. True = Enabled
+    tw = true;
+    cn = false;
+    us = false;
+    // UI parameter.
     isSearching = false;
     isSearched = false;
-    isLegacy = false;
     isDetail = false;
-    detailType;
-    detailContent;
+    // Detail page data binding.
+    detailData: { type: string, content: ParsedContent[] };
 
     constructor() {
         super();
-        this.token = Random.id();
+        this.queryToken = Random.id();
         this.subscribe("results", () => {
-            this.results = Results.find({ _id: this.token });
+            this.results = Results.find({ _id: this.queryToken });
             this.autorun(() => this.assignKeyToAggArray(), true);
         });
     }
@@ -34,52 +38,60 @@ export class Main extends MeteorComponent {
             this.aggs = [];
             let aggs = this.results.fetch()[0].aggs;
             for (var key in aggs)
-                //assign the first eight aggs.
+                // Assign the first eight aggs.
                 this.aggs.push({ "key": key, "val": aggs[key].slice(0, 8) });
         }
     }
 
-    setDefaultScrollPosition() {
-        $("html").scrollTop(0);
-    }
-
     back() {
-        if (!this.isDetail) {
-            this.isSearching = false;
-            this.isSearched = false;
-            $("#legacy").click();
-            $("#search").focus();
-        } else {
-            this.setDefaultScrollPosition();
-            this.isDetail = false;
-        }
+        if (!this.isDetail)
+            this.transitionsContentPage(".result-wrapper", ".content-wrapper");
+        else
+            this.transitionsResultPage(".detail-wrapper", ".analyze-wrapper");
     }
 
     search(text) {
         if (text) {
-            this.setDefaultScrollPosition();
             $("#search").blur();
             this.isSearching = true;
-            Meteor.call("search", text, this.countryCheck(), this.token, () => {
-                this.isSearching = false;
-                this.isSearched = true;
-                this.isLegacy = true;
-            });
+            Meteor.call("search", text, this.countryCheck(), this.queryToken, () => this.transitionsContentPage(".content-wrapper", ".result-wrapper"));
         }
     }
 
     countryCheck(): string[] {
         var countries: string[] = [];
-        if (!this.tw) countries.push("TW");
-        if (!this.cn) countries.push("CN");
-        if (!this.us) countries.push("US");
+        if (this.tw) countries.push("TW");
+        if (this.cn) countries.push("CN");
+        if (this.us) countries.push("US");
         return countries
     }
 
     showDetail(key: string) {
-        this.setDefaultScrollPosition();
-        this.detailType = key;
-        this.detailContent = this.results.fetch()[0].aggs[key];
-        this.isDetail = true;
+        this.detailData = {
+            type: key,
+            content: this.results.fetch()[0].aggs[key]
+        };
+        this.transitionsResultPage(".analyze-wrapper", ".detail-wrapper");
+    }
+    
+    transitionsContentPage(current: string, next: string) {
+        $(current).animate({ opacity: 0, easing: "easeOutQuart" }, 300, () => $(current).hide());
+        if (this.isSearched)
+            $(next).show(() => $("#search").focus()).animate({ opacity: 1, easing: "easeOutQuart" }, 300);
+        else {
+            $(next).show().animate({ opacity: 1, easing: "easeOutQuart" }, 300);
+            $(".analyze-wrapper").scrollTop(0);
+        }
+        this.isSearching = false;
+        this.isSearched = !this.isSearched;
+    }
+    
+    transitionsResultPage(current: string, next: string) {
+        $(current).animate({ opacity: 0, easing: "easeOutQuart" }, 300, () => $(current).hide());
+        if (this.isDetail)
+            $(next).show().animate({ opacity: 1, easing: "easeOutQuart" }, 300);
+        else
+            $(next).show().scrollTop(0).animate({ opacity: 1, easing: "easeOutQuart" }, 300);
+        this.isDetail = !this.isDetail;
     }
 }
